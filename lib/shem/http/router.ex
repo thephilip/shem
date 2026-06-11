8cb143770aa47fd20a4bf1e1d4072cc410051fae
Philip <13875805+thephilip@users.noreply.github.com>
2026-06-11 10:44:36 -0400

    feat: serve timeline.html at GET /timeline

--- lib/shem/http/router.ex
+++ lib/shem/http/router.ex
@@ -20,6 +20,13 @@ defmodule Shem.HTTP.Router do
     |> send_file(200, path)
   end
 
+  get "/timeline" do
+    path = Application.app_dir(:shem, "priv/static/timeline.html")
+    conn
+    |> put_resp_content_type("text/html")
+    |> send_file(200, path)
+  end
+
   match _ do
     conn
     |> put_resp_content_type("application/json")
